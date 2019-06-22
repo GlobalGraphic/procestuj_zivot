66e0303dc7cea091a3c197757f5d5c4cdd7933a3
codeHysteria <graphicglobal67@gmail.com>
2019-06-22 08:52:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="cz">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Nabízíme levné dovolené, akciové letenky a nejzajímavější dárky pro muze">
+<meta name="robots" content="index, follow">
+<meta name="keywords" content="letenky,lacne letenky,affiliate,lacne dovolenky,dovolenky">
+<meta name="author" content="Global Graphic & Design">
+<meta http-equiv="X-UA-cCmpatible" content="IE=edge, chrome=1">
+<meta name="google-site-verification" content="uTqTKB4NzJsVcX8HflA3w_v7t6ovtFOgmyNM3jGAHIM" />
+<link rel="stylesheet" href="style.css?v=<?=time();?>">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<title>Procestuj Život</title>
+</head>
+<body>
+<div class="main">
+
+
+<nav id="menu" class="navbar navbar-expand-md navbar-light">
+  <a class="navbar-brand" href="index.php">
+    <img src="logo.png" alt="loginko" style="width: 200px;" />
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link hvr-underline-from-center" href="#heading">Dovolená</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link hvr-underline-from-center" href="#letenky">Letenky</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link hvr-underline-from-center" href="#promuze">Zážitky pro muže</a>
+      </li>
+    </ul>
+  </div>  
+</nav>
+
+
+
+<div id="demo" class="carousel slide" data-ride="carousel" data-interval="3000">
+
+  <!-- The slideshow -->
+  <div class="carousel-inner text-center">
+    <div class="carousel-item active">
+      <img src="img/tropical-holiday-1.jpg" alt="holiday" class="img-fluid" style="opacity: 0.4;">
+        <div class="carousel-caption">
+          <h3 style="color: #8D1B7F;">Přehled nejlevnějších dovolených</h3><br>
+          <a href="https://affil.travelportal.cz/redirect.php?aid=382&cid=40444" rel="nofollow" target="_blank"><button class="btn text-white" style="background-color: #8D1B7F;">Více info</button></a>
+      </div>  
+    </div>
+    <div class="carousel-item">
+      <img src="img/d15b370d-hero.jpg" alt="hol" class="img-fluid" style="opacity: 0.4;">
+      <div class="carousel-caption">
+        <h3 style="color: #8D1B7F;">Přehled nejlevnějších letenek</h3><br>
+        <a href="https://www.pelikan.cz/cs" rel="nofollow" target="_blank"><button class="btn text-white" style="background-color: #8D1B7F;">Více info</button></a>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/bjvholi_0.jpg" alt="lol" class="img-fluid" style="opacity: 0.4;">
+      <div class="carousel-caption">
+        <h3 style="color: #8D1B7F;">ZÁÁÁÁŽITKOVÉ DÁRKY PRO CHLAPY</h3><br>
+       <a href="http://www.adrop.cz?a_box=ybda96b7" rel="nofollow" target="_blank"><button class="btn text-white" style="background-color: #8D1B7F;">Více info</button></a>
+    </div>
+    </div>
+  
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+  </div>
+</div>
+<br><br><br>
+
+<h1 class="text-center" id="heading">Naše tipy na skvělou dovolenou</h1>
+<br><br><br><br><br>
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-4 text-center">
+       <h2>Egypt</h2><br><br>
+       <img src="img/27150677.jpg" alt="egypt" class="img-fluid"><br><br>
+        <a href="https://affil.travelportal.cz/redirect.php?aid=382&cid=37744" rel="nofollow" target="_blank"><button class="btn text-white" style="background-color: #8D1B7F;">Více info</button></a>
+
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Řecko</h2><br><br>
+       <img src="img/shutterstock_125504618.jpg" alt="grecko" class="img-fluid"><br><br>
+        <a href="https://affil.travelportal.cz/redirect.php?aid=382&cid=37743" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">Více info</button></a>
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Turecko</h2><br><br>
+       <img src="img/turecko-5_1200x0.jpg" alt="turecko" class="img-fluid"><br><br>
+       <a href="https://affil.travelportal.cz/redirect.php?aid=382&cid=37745" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">Více info</button></a>
+    </div>
+
+  </div>
+</div>
+
+<br><br><br>
+
+<div class="col text-center"> <a href="https://affil.travelportal.cz/redirect.php?aid=382&cid=38735" rel="nofollow" target="_blank"><button class="btn text-dark mx-auto" style="background-color: #ffdb34;">Načíst další nabídky</button></a></div>
+
+
+<br><br><br><br><br><br>
+
+<h2 class="text-center heading">Exotický ráj za dostupnou cenu</h2>
+<br><br><br><br><br>
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-4 text-center">
+       <h2>Emiráty</h2><br><br>
+       <img src="https://s3.eu-central-1.amazonaws.com/mytravel-world/uploads/2016/11/03/529e2c608fe36ce9f4c1348c80e9a82048de39e5.jpg" alt="emiraty" class="img-fluid"><br><br>
+        <a href="https://affil.travelportal.cz/redirect.php?aid=382&cid=37749" rel="nofollow" target="_blank"><button class="btn text-white" style="background-color: #8D1B7F;">od 6990 Kč</button></a>
+
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Maledivy</h2><br><br>
+       <img src="https://g.denik.cz/104/80/maledivy-hlavni-foto_ng-detail-photo-p.jpg" alt="maledivy" class="img-fluid" style="height: 233px;"><br><br>
+        <a href="https://affil.travelportal.cz/redirect.php?aid=382&cid=37750" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 28 990 Kč</button></a>
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Thajsko</h2><br><br>
+       <img src="https://cdn.fischer.cz/Images/000380/thajsko-0_1200x0.jpg" alt="thajsko" class="img-fluid"><br><br>
+       <a href="https://affil.travelportal.cz/redirect.php?aid=382&cid=37751" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 16 179 Kč</button></a>
+    </div>
+  </div>
+</div>
+
+<br><br><br>
+
+
+
+<br><br><br><br><br><br>
+
+<h2 class="text-center heading" id="letenky"> Akční letenky</h2>
+<br><br><br><br><br>
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-4 text-center">
+       <h2>Řím</h2><br><br>
+       <img src="https://cdn.escapismmagazine.com/gallery/5899b8dd8af91.jpeg" alt="rim" class="img-fluid"><br><br>
+        <a href="https://www.pelikan.cz/cs/akcni-letenka/LCC-281" rel="nofollow" target="_blank"><button class="btn text-white" style="background-color: #8D1B7F;">od 743 Kč</button></a>
+
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Palma de Mallorca</h2><br><br>
+       <img src="https://cdn.traghetti.com/porti/palma-de-mallorca195-20983.jpg" alt="mallorca" class="img-fluid"><br><br>
+        <a href="https://www.pelikan.cz/cs/akcni-letenka/LCC-958" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 743 Kč</button></a>
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Paříž</h2><br><br>
+       <img src="https://p2d7x8x2.stackpathcdn.com/wordpress/wp-content/uploads/2014/10/france-wallpaper-paris-eiffel-tower-hd-desktop-l-a-ibackgroundz.com_.jpg" alt="paris" class="img-fluid"><br><br>
+       <a href="https://www.pelikan.cz/cs/akcni-letenka/G-9298" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 2 290 Kč</button></a>
+    </div>
+  </div>
+</div>
+
+<br><br><br>
+
+<div class="col text-center"> <a href="https://www.pelikan.cz/cs/akcni-letenky/" rel="nofollow" target="_blank"><button class="btn text-dark mx-auto" style="background-color: #ffdb34;">Zobrazit celou nabídku</button></a></div>
+
+
+<br><br><br><br><br><br>
+
+<h2 class="text-center heading" id="promuze"> Zážitky pro muže</h2>
+<br><br><br><br><br>
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-4 text-center">
+       <h2>Pirát silnic</h2><br><br>
+       <img src="https://files.adrop.cz/files/cats/322/135-135/piratsilnic-1-homepage-webdesign-adrop.jpg" alt="rim" class="img-fluid"><br><br>
+        <a href="https://www.adrop.cz/darky-pro-ridice?a_box=ybda96b7" rel="nofollow" target="_blank"><button class="btn text-white" style="background-color: #8D1B7F;">od 890 Kč</button></a>
+
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Sportovec</h2><br><br>
+       <img src="https://files.adrop.cz/files/cats/323/135-135/sportovec-1-homepage-webdesign-adrop.jpg" alt="mallorca" class="img-fluid"><br><br>
+        <a href="https://www.adrop.cz/darky-pro-sportovce?a_box=ybda96b7" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 790 Kč</button></a>
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Drsňák</h2><br><br>
+       <img src="https://files.adrop.cz/files/cats/324/135-135/drsnak-1-homepage-webdesign-adrop.jpg" alt="paris" class="img-fluid"><br><br>
+       <a href="https://www.adrop.cz/adrenalinove-zazitky-pro-muze?a_box=ybda96b7" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 750 Kč</button></a>
+    </div>
+
+    
+  </div>
+</div>
+
+
+<br><br><br><br><br>
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-4 text-center">
+      <h2>Gentleman</h2><br><br>
+       <img src="https://files.adrop.cz/files/cats/356/135-135/gentleman-charaktery-1-homepage-webdesign-adrop.jpg" alt="paris" class="img-fluid"><br><br>
+       <a href="https://www.adrop.cz/stylove-darky-pro-gentlemany?a_box=ybda96b7" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 600 Kč</button></a>
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Pohodář</h2><br><br>
+       <img src="https://files.adrop.cz/files/cats/357/135-135/pohodar-charaktery-1-homepage-webdesign-adrop.jpg" alt="paris" class="img-fluid"><br><br>
+       <a href="https://www.adrop.cz/darky-pro-pohodare?a_box=ybda96b7" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 329 Kč</button></a>
+    </div>
+
+    <div class="col-md-4 text-center">
+      <h2>Hračička</h2><br><br>
+       <img src="https://files.adrop.cz/files/cats/324/135-135/drsnak-1-homepage-webdesign-adrop.jpg" alt="paris" class="img-fluid"><br><br>
+       <a href="https://www.adrop.cz/hrave-darky-pro-muze?a_box=ybda96b7" rel="nofollow" target="_blank"><button class="btn text-white"  style="background-color: #8D1B7F;">od 1 500 Kč</button></a>
+    </div>
+    
+  </div>
+</div>
+
+
+<br><br><br>
+
+<div class="col text-center"> <a href="https://www.adrop.cz/darkovy-pruvodce?a_box=ybda96b7" rel="nofollow" target="_blank"><button class="btn text-dark mx-auto" style="background-color: #ffdb34;">Přejít do dárkového průvodce</button></a></div>
+
+
+<br><br>
+<footer class="page-footer font-small bg-light lighten-3 pt-4">
+    <div class="footer-copyright text-center py-3 text-secondary">
+      © procestujzivot.cz 2019 Všechny práva vyhrazena<br>
+      <a href="https://globalgraphicdesign.eu" rel="follow" target="_blank" style="color: #8D1B7F;">Vytvořil: Global Graphic & Design</a>
+    </div>
+</footer>
+</div>
+<script>
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); 
+            $target.focus(); 
+          };
+        });
+      }
+    }
+  });
+</script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
